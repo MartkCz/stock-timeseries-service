d5cfe52e8db56dc5a8cff91b2dc2ecdda3d3696c
MartkCz <martk494@gmail.com>
2024-09-21 12:33:57 +0200

    feat(timeseries): add multiple time-series endpoint

--- src/StockTimeSeriesService.php
+++ src/StockTimeSeriesService.php
@@ -9,10 +9,22 @@ final class StockTimeSeriesService extends Service
 {
 
 	public const TimeSeriesLink = '/api/timeseries-service/time-series';
+	public const MultipleTimeSeriesLink = '/api/timeseries-service/time-series/multiple';
 
 	public function timeSeries(string $symbol, TimeSeriesRange $range): ServiceRequest
 	{
 		return $this->requestGet(self::TimeSeriesLink . '/' . $symbol, [
+			'range' => $range->value,
+		]);
+	}
+
+	/**
+	 * @param string[] $symbols
+	 */
+	public function multipleTimeSeries(array $symbols, TimeSeriesRange $range): ServiceRequest
+	{
+		return $this->requestGet(self::MultipleTimeSeriesLink, [
+			'symbols' => implode(',', $symbols),
 			'range' => $range->value,
 		]);
 	}
